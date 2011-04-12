56e1e144e574371732a6edfa6137ce6a099563e7
Ben Davis <bendavis78@gmail.com>
2011-04-12 17:36:49 -0500

    updated PostAdmin

--- forum/admin.py
+++ forum/admin.py
@@ -14,7 +14,18 @@ class ThreadAdmin(admin.ModelAdmin):
     list_display = ('title', 'forum', 'latest_post_time')
     list_filter = ('forum',)
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('_author', 'time', '_forum', 'thread')
+    list_filter = ('thread__forum',)
+    search_fields = ('author__email', 'body', 'thread__title', 'thread__forum__title')
+
+    def _forum(self, obj):
+        return obj.thread.forum
+    
+    def _author(self, obj):
+        return obj.author.email
+
 admin.site.register(Forum, ForumAdmin)
 admin.site.register(Thread, ThreadAdmin)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
